8880ae06a5cab28fe7ee85a234f9e8695af024c4
Alejo <alejosegi@gmail.com>
2020-09-21 22:03:34 +0200

    Corrección para metodo AJAX

--- main.js
+++ main.js
@@ -2,9 +2,29 @@ $('#contactForm').on('submit',function(e){
     //optional validation code here
 
     e.preventDefault();
-  
+
     $.ajax({
-        url: "https://script.google.com/macros/s/AKfycbxuSCQS7maeU-2N3E7T0fgqGlby5bGOEmLSvmh2/exec",
+        url: "https://script.google.com/macros/s/AKfycbzunX748Nd0UFy9kA2KM3DUrDpuBJJfeOTRL7eZ-2ha2Gpc5uI/exec",
+        method: "POST",
+        dataType: "json",
+        data: $("#contactForm").serialize(),
+    }).done(function (response) {
+        
+        if(response.result == "success") {
+            $('#contactForm')[0].reset();
+            alert('Gracias por contactarte con nosotros!.');
+            return true;
+        } else {
+            alert("Algo salio mal. por favor intentalo de nuevo.");
+        }
+
+    }).fail(function (error) {
+        alert("Algo salio mal. por favor intentalo de nuevo.");
+        console.log(error);
+    });
+  
+    /* $.ajax({
+        url: "https://script.google.com/macros/s/AKfycbzunX748Nd0UFy9kA2KM3DUrDpuBJJfeOTRL7eZ-2ha2Gpc5uI/exec",
         method: "POST",
         dataType: "json",
         data: $("#contactForm").serialize(),
@@ -23,5 +43,5 @@ $('#contactForm').on('submit',function(e){
             
             alert("Algo salio mal. por favor intentalo de nuevo.")
         }
-    })
+    }) */
 });
