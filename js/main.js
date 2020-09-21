3fcd3805a5180b11ab58f36637f8fbf465167b23
Alejo <alejosegi@gmail.com>
2020-09-21 22:35:14 +0200

    Actualización de script contactForm

--- js/main.js
+++ js/main.js
@@ -20,29 +20,7 @@ $('#contactForm').on('submit',function(e){
         }
 
     }).fail(function (error) {
-        alert("Algo salio mal. por favor intentalo de nuevo.");
         console.log(error);
     });
-  
-    /* $.ajax({
-        url: "https://script.google.com/macros/s/AKfycbzunX748Nd0UFy9kA2KM3DUrDpuBJJfeOTRL7eZ-2ha2Gpc5uI/exec",
-        method: "POST",
-        dataType: "json",
-        data: $("#contactForm").serialize(),
-        success: function(response) {
-            
-            if(response.result == "success") {
-                $('#contactForm')[0].reset();
-                alert('Gracias por contactarte con nosotros!.');
-                return true;
-            }
-            else {
-                alert("Algo salio mal. por favor intentalo de nuevo.")
-            }
-        },
-        error: function() {
-            
-            alert("Algo salio mal. por favor intentalo de nuevo.")
-        }
-    }) */
+    
 });
